52f075af189ac0a32af936d596209bbdba83bf69
Tyler Fahey <twfahey1@gmail.com>
2021-10-25 13:58:29 +0000

    Remove devcontainer

--- .devcontainer/local-dev-files/mysql-init.sh
+++
@@ -1,5 +0,0 @@
-sudo mysql -u root<<EOFMYSQL
-    CREATE USER 'user'@'localhost' IDENTIFIED BY '1234'; 
-    GRANT ALL PRIVILEGES ON *.* TO 'user'@'localhost' WITH GRANT OPTION;
-    CREATE DATABASE IF NOT EXISTS drupal;
-EOFMYSQL
